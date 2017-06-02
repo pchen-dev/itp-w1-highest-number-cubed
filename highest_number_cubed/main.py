c13a4a0171cda81893bfc5c746248b51c4d6c368
Ping Chen <pxx_chen@yahoo.com>
2017-06-02 01:22:54 +0000

    DONE!

--- highest_number_cubed/main.py
+++ highest_number_cubed/main.py
@@ -1,5 +1,19 @@
 """This is the entry point of the program."""
 
 
+
+
 def highest_number_cubed(limit):
-    pass
+    
+    constant = 1 
+    result = 0
+    
+    while result < limit:
+        result = constant ** 3
+        
+        if result < limit:
+            constant += 1
+        if result > limit:
+            constant = constant -1
+            return constant
+
